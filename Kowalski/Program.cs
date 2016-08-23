da78989bbdfb441864b14d5f1298c6c5660feee4
davidemoraschi <davidem@eurostrategy.net>
2016-08-23 15:02:56 +0200

    moved to GitHub

---
+++ Kowalski/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kowalski
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (NorthwindEFModelContext context = new NorthwindEFModelContext())
+            {
+                foreach (var c in context.Customers.Where(c => c.City == "Seattle"))
+                {
+                    Console.WriteLine(c.CompanyName);
+                }
+            }
+        }
+    }
+}
